71e83e7e8a2978c26862f985d753f536fd600afe
Codexive_zech <zechariahhounwanou@gmail.com>
2022-11-09 02:42:02 +0100

    Added new Project and update CV

--- src/utils/project.js
+++ src/utils/project.js
@@ -1,3 +1,4 @@
+import homeySoftStore from "../images/HomeySoft.png";
 import moviesApp from "../images/movies-app.png";
 import jobstrApp from "../images/jobstr.png";
 import tintShadeColor from "../images/tint-shade-color.png";
@@ -9,27 +10,36 @@ const projects = [
     image: jobstrApp,
     name: "Jobster",
     description:
-      "Jobstr is a powerful and easy job tracking application that helps users to manage jobs, organize jobs and tracking of jobs app for users",
+      "Jobstr is a powerful and easy job tracking application that helps users to manage jobs, organize jobs and tracking of jobs app for users.",
   },
 
   {
     id: 2,
+    url: "https://homeysoft.netlify.app/",
+    image: homeySoftStore,
+    name: "Homey Soft Store",
+    description:
+      "Homey Soft is an E-commerce store which enable users to buy varieties of comfortable home furniture for personal use.",
+  },
+
+  {
+    id: 3,
     url: "https://chic-movies.netlify.app",
     image: liliesFood,
     name: "Lilies Food App",
     description:
-      "Lilies Food is a food ordering app where user can order for food they which to have and delivered to them.",
+      "Lilies Food is a food ordering app which allows users to order for delicious meals they are craving for and have it delivered to them.",
   },
   {
-    id: 3,
+    id: 4,
     url: "https://chic-movies.netlify.app",
     image: moviesApp,
     name: "Chic Movies App",
     description:
-      "Chic Movies is a free movie app where user can find most of the latest movies, top rated movies as well as the upcoming movies so as to always be closer to movies",
+      "Chic Movies is a free movie app where user can find most of the latest movies, top rated movies as well as the upcoming movies so as to always be closer to movies.",
   },
   {
-    id: 3,
+    id: 5,
     url: "https://lightanddarkcolorgen.netlify.app",
     image: tintShadeColor,
     name: "Tint and Shades Color",
